9618192448b5346fddb27ab0a00b4d2e08cadfbf
thiyagarajan2002 <64621469+thiyagarajan2002@users.noreply.github.com>
2021-06-27 12:03:07 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Linux History By TRJ TECH</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Linux History</h1>
+      <p>
+        <a href="https://en.wikipedia.org/wiki/Linux">click here</a>
+      </p>
+    </div>
+  );
+}
+
+export default Info;
